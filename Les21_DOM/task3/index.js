909d6aab0ba8cd73832f3856f3ec4dc407aedc92
AnnaGolitsyna <110576gav@gmail.com>
2022-11-16 18:33:34 +0200

    task3

---
+++ Les21_DOM/task3/index.js
@@ -0,0 +1,26 @@
+// algo
+// use m querySelectorAll
+// create f getItemsList
+// * find all el .technology => dir, return
+// create f getItemsArray
+// * find all el .tool => add[] => dir, return
+// * use Array.from(NodeList)
+// export f
+
+const getItemsList = () => {
+    const elementsList = document.querySelectorAll('.technology');
+    console.dir(elementsList);
+    return elementsList;
+}
+
+getItemsList();
+
+const getItemsArray = () => {
+    const elementsArray = document.querySelectorAll('.tool');
+    console.dir(elementsArray);
+    return Array.from(elementsArray);
+}
+
+getItemsArray();
+
+export {getItemsArray, getItemsList}
