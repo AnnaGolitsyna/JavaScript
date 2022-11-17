682c0f5aa1528135c958652a6b1544dca2d08e0a
AnnaGolitsyna <110576gav@gmail.com>
2022-11-17 15:44:02 +0200

    task9

---
+++ Les21_DOM/task9/index.js
@@ -0,0 +1,23 @@
+// algo
+// create f finishList
+// find el .list
+// iterate .list => if don't find num - create/append him + text/num/
+// * list = 1-8
+// export f
+
+export const finishList = () => {
+  const listElem = document.querySelector('.list');
+  const listItemElem = document.querySelectorAll('li');
+  console.log(listItemElem);
+  const newItemElem = document.createElement('li');
+  console.log(newItemElem);
+  listElem.prepend((newItemElem.textContent = '1'));
+  console.log(listElem);
+  const middleItem = document.querySelector('.special');
+  console.log(middleItem);
+  middleItem.before((newItemElem.textContent = '4'));
+  middleItem.after((newItemElem.textContent = '6'));
+  listElem.append((newItemElem.textContent = '8'));
+};
+
+finishList();
