078f1b8b7c6f0567990755c1b622d6392a47bc55
AnnaGolitsyna <110576gav@gmail.com>
2022-11-17 08:41:29 +0200

    task5

---
+++ Les21_DOM/task5/index.js
@@ -0,0 +1,13 @@
+// algo
+// use m querySelector
+// create f setTitle => input - 'text' string
+// * find el .title => add 'text', use textContent
+// export f
+
+export const setTitle = text => {
+  document.querySelector('.title').textContent = text;
+};
+
+setTitle('Anna');
+
+
