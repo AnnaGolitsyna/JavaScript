f7d23f0a1ecb5cedcdd0fdd6a4b34bdc510a1287
AnnaGolitsyna <110576gav@gmail.com>
2022-11-18 06:49:31 +0200

    refactoring

--- Les21_DOM/task9/index.js
+++ Les21_DOM/task9/index.js
@@ -1,30 +1,27 @@
 // algo
 // create f finishList
 // find el .list
-// iterate .list => if don't find num - create/append him + text/num/
-// * list = 1-8
+// create f => create el newLi
+// * append new li = 1-8
 // export f
 
 export const finishList = () => {
   const listElem = document.querySelector('.list');
   const listItemElem = document.querySelectorAll('li');
 
-  const newLi1 = document.createElement('li');
-  newLi1.textContent = '1';
-  listElem.prepend(newLi1);
+  const newLi = text => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    return li;
+  };
+
+  listElem.prepend(newLi('1'));
 
   const middleItem = document.querySelector('.special');
-  const newLi4 = document.createElement('li');
-  newLi4.textContent = '4';
-  middleItem.before(newLi4);
+  middleItem.before(newLi('4'));
+  middleItem.after(newLi('6'));
 
-  const newLi6 = document.createElement('li');
-  newLi6.textContent = '6';
-  middleItem.after(newLi6);
-
-  const newLi8 = document.createElement('li');
-  newLi8.textContent = '8';
-  listElem.append(newLi8);
+  listElem.append(newLi('8'));
 };
 
-// finishList();
+finishList();
