030a43aaf5b1b3144c1d4ab157a9a7c327d4e0b7
AnnaGolitsyna <110576gav@gmail.com>
2022-11-18 07:43:33 +0200

    task10

--- Les21_DOM/task10/index.js
+++ Les21_DOM/task10/index.js
@@ -1,1 +1,18 @@
 // algo
+// create f finishForm /export +++
+// *prepend input 'name='Login'' +++
+// *change type => password +++
+
+export const finishForm = () => {
+  const form = document.querySelector('.login-form');
+
+  const inputPassw = document.querySelector('input');
+
+  inputPassw.setAttribute('type', 'password');
+
+  const inputLogin = document.createElement('input');
+  inputLogin.setAttribute('type', 'text');
+  inputLogin.setAttribute('name', 'login');
+  form.prepend(inputLogin);
+};
+// finishForm();
