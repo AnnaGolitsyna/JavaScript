e0c7c7bec8cae0703a31962011153b20ec298ba6
AnnaGolitsyna <110576gav@gmail.com>
2022-11-18 16:19:49 +0200

    value

--- Les21_DOM/task13/index.js
+++ Les21_DOM/task13/index.js
@@ -4,13 +4,13 @@
 // => output str 'value' /data-section/ pearents cl .box
 
 export const getSection = num => {
-
   const el = document.querySelector(`span[data-number='${num}']`);
   console.log(el);
   const pearent = el.closest('.box');
   console.log(pearent, pearent.dataset);
-  return pearent.dataset;
-
+  const pearentsValue = pearent.dataset.section;
+  console.log(pearentsValue);
+  return pearentsValue;
 }
 // getSection('3');
 // getSection('5');
