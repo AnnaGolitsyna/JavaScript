6949da32f2eda23c2a3cbe6f75b2e034af354e48
AnnaGolitsyna <110576gav@gmail.com>
2022-11-20 14:48:33 +0200

    changeRemove

--- Les22_Evtnts/task1/index.js
+++ Les22_Evtnts/task1/index.js
@@ -24,7 +24,7 @@ const logGreenP = logTarget.bind(null, 'P', 'green');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
 
-function handleClick() {
+function addHandleClick() {
   divElem.addEventListener('click', logGreyDiv, true);
   divElem.addEventListener('click', logGreenDiv);
 
@@ -37,16 +37,27 @@ function handleClick() {
 
 const clear = () => {
   const eventsListElem = document.querySelector('.events-list');
-  eventsListElem.replaceChildren();
+  eventsListElem.innerHTML = '';
 };
 
 const butClear = document.querySelector('.clear-btn');
 butClear.addEventListener('click', clear);
 
-const butAttach = document.querySelector('.attach-handlers-btn');
-butAttach.addEventListener('click', handleClick);
 
-// handleClick();
+const butAttach = document.querySelector('.attach-handlers-btn');
+butAttach.addEventListener('click', addHandleClick);
+
+function removeHandleClick() {
+  divElem.removeEventListener('click', logGreyDiv, true);
+  divElem.removeEventListener('click', logGreenDiv);
+
+  pElem.removeEventListener('click', logGreyP, true);
+  pElem.removeEventListener('click', logGreenP);
+
+  spanElem.removeEventListener('click', logGreySpan, true);
+  spanElem.removeEventListener('click', logGreenSpan);
+}
 
 const butRemove = document.querySelector('.remove-handlers-btn');
-butRemove.removeEventListener('click', handleClick);
+butRemove.addEventListener('click', removeHandleClick);
+// handleClick();
