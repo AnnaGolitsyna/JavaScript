d95c98f32d6572e9a1c5dc025fb5fe7e7290f8bc
AnnaGolitsyna <110576gav@gmail.com>
2022-11-20 15:40:54 +0200

    task4

---
+++ Les22_Evtnts/task4/index.js
@@ -0,0 +1,9 @@
+// algo
+// create change => checkbox .task-status
+
+const checkboxElem = document.querySelector('.task-status');
+
+checkboxElem.addEventListener('change', () => {
+    console.log(`${checkboxElem.checked}`);
+    // console.log(`${checkboxElem.value}`);
+})
