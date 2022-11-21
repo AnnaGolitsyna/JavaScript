bdd85bb797b3f30712296f910104095b58acc1da
AnnaGolitsyna <110576gav@gmail.com>
2022-11-21 08:17:16 +0200

    task8

---
+++ Les22_Events/task8/index.js
@@ -0,0 +1,8 @@
+// algo
+// creat change => .text-input
+
+const inputEl = document.querySelector('.text-input');
+
+inputEl.addEventListener('change', (event) => {
+  console.log(event.target.value);
+})
