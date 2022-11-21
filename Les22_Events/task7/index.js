8006896f946af6a5bb033b6c49eae6a40220bb5d
AnnaGolitsyna <110576gav@gmail.com>
2022-11-21 08:00:15 +0200

    task7

--- Les22_Events/task7/index.js
+++ Les22_Events/task7/index.js
@@ -1,7 +1,12 @@
 // algo
+// create click => both btn
+// create f hendleClick => both btn
 
 
+function handleClick(event) {
+  console.log(event.target.textContent);
+}
 
-
-
-
+const butElems = document.querySelectorAll('.btn');
+const arrButElems = [...butElems];
+arrButElems.forEach(btn => btn.addEventListener('click', handleClick));
