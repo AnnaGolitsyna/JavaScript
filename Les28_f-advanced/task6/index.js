4bd78780964f68f5e3bd91d838165fbb9b8f68d2
AnnaGolitsyna <110576gav@gmail.com>
2022-12-04 13:19:44 +0200

    fibonacci

--- Les28_f-advanced/task6/index.js
+++ Les28_f-advanced/task6/index.js
@@ -1,1 +1,23 @@
 // algo
+// create f - export =>
+// input - number > 0
+// output - max numFib < number
+// create let numFib
+// find all numFib => add to let numFib
+
+let firstNum = 0;
+let secondNum = 1;
+let sumFib = 0;
+
+export const maxFibonacci = num => {
+  if (firstNum + secondNum <= num) {
+    sumFib = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = sumFib;
+    sumFib = maxFibonacci(num);
+  }
+
+  return sumFib;
+};
+// console.log(maxFibonacci(15));
+console.log(maxFibonacci(71));
