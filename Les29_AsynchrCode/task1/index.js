6eca70d6e6849dd012b4c9fe1baff11b7c4c916f
AnnaGolitsyna <110576gav@gmail.com>
2022-12-06 14:20:29 +0200

    refactoring

--- Les29_AsynchrCode/task1/index.js
+++ Les29_AsynchrCode/task1/index.js
@@ -15,16 +15,18 @@ export const addImage = (imgSrc, callback) => {
   containerElem.append(imgElem);
 
   const getImageLoaded = () => {
-    const { width, height } = imgElem;
-    callback(null, { width, height });
+    callback(null, imgElem);
   };
 
   imgElem.addEventListener('load', getImageLoaded);
   imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
+const imageSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
 // callack function
-const onImageLoaded = (error, imgElem) => {
+export const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
@@ -32,12 +34,8 @@ const onImageLoaded = (error, imgElem) => {
 
   const { width, height } = imgElem;
   const sizeElem = document.querySelector('.image-size');
-
   sizeElem.textContent = `${width} x ${height}`;
 };
 
 // examples
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+addImage(imageSrc, onImageLoaded);
