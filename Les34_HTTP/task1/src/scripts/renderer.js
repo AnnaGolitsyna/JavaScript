b6c5d0b607044b003ba6c2997fe274391c467ddc
AnnaGolitsyna <110576gav@gmail.com>
2022-12-18 17:17:52 +0200

    render

--- Les34_HTTP/task1/src/scripts/renderer.js
+++ Les34_HTTP/task1/src/scripts/renderer.js
@@ -2,24 +2,38 @@ import { getTasksList } from './tasksGateway.js';
 
 const listElem = document.querySelector('.list');
 
+const addAttrCheckbox = (elem, done, id) => {
+  elem.setAttribute('type', 'checkbox');
+  elem.classList.add('list-item__checkbox');
+  elem.checked = done;
+  elem.dataset.id = id;
+};
+
+const addAttrText = (elem, text) => {
+  elem.classList.add('list-item__text');
+  elem.textContent = text;
+};
+
+const addAttrDeleteBtn = (elem, id) => {
+  elem.classList.add('list-item__delete-btn');
+  elem.dataset.id = id;
+};
+
 export const creatListElem = ({ text, done, id }) => {
   const listItemElem = document.createElement('li');
   listItemElem.classList.add('list-item', 'list__item');
+
   const checkbox = document.createElement('input');
-  checkbox.setAttribute('type', 'checkbox');
-  checkbox.checked = done;
-  checkbox.classList.add('list-item__checkbox');
-  checkbox.dataset.id = id;
+  addAttrCheckbox(checkbox, done, id);
   if (done) {
     listItemElem.classList.add('list-item_done');
   }
   const textElem = document.createElement('span');
-  textElem.classList.add('list-item__text');
-  textElem.textContent = text;
+  addAttrText(textElem, text);
 
   const deleteBtnElem = document.createElement('button');
-  deleteBtnElem.classList.add('list-item__delete-btn');
-  deleteBtnElem.dataset.id = id;
+  addAttrDeleteBtn(deleteBtnElem, id);
+  
   listItemElem.append(checkbox, textElem, deleteBtnElem);
   return listItemElem;
 };
